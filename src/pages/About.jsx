309653b26aa8fb7a338052bc071eaae8f33fb675
SenixDev <senixdev14yo@gmail.com>
2026-03-31 20:01:49 +0500

    feat: add profile photo to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
               transition={{ duration: 0.6 }}
             >
               <div className="avatar">
-                <div className="placeholder-avatar">👨‍💻</div>
+                <img src="/icon.jpg" alt="Senix" />
               </div>
               <div className="social-links-about">
                 {socialLinks.map((social, index) => (
